17244308baee13da211ef84b5b89d0f05aba2f22
EunJeong Lee <sssver28@gmail.com>
2021-04-29 17:14:54 +0900

    add sample

---
+++ sample.js
@@ -0,0 +1,20 @@
+// 문제요약
+
+
+// 입출력 예시 
+
+
+// ? 수도코드
+
+
+// 통과한 코드 (21.04.29)
+// 시간복잡도 O(N) / 모든요소를 다 한번씩 확인해서
+function solution(arr) {
+
+}
+
+
+// 실행코드
+console.log(solution([1, 1, 3, 3, 0, 1, 1]));
+
+// 링크 : https://programmers.co.kr/learn/courses/30/lessons/12906
