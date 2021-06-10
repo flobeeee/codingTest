8447d9d882390762cad71f79d167c86f33ddf267
flobeeee <sssver28@gmail.com>
2021-06-10 20:40:47 +0900

    add 29 in Level_1

--- Level_1/29.desSortNum.js
+++ Level_1/29.desSortNum.js
@@ -10,14 +10,13 @@
 // 내림차순으로 sort 를 한다.
 
 // 통과한 코드 (21.06.10)
-// 시간복잡도 O(n) O(n²) O(n log n) / 이유
+// 시간복잡도 O(n²) / sort 는 숫자끼리 전부 비교하는 작업이라서 이중 for 문과 같다.
 function solution(n) {
-    var answer = 0;
-    return answer;
+  return Number(String(n).split("").sort((a, b) => b - a).join(""))
 }
 
 
 // 실행코드
-console.log(solution());
+console.log(solution(118372));
 
 // 링크 : https://programmers.co.kr/learn/courses/30/lessons/12933?language=javascript
