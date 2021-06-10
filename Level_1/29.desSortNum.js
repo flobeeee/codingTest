b680ce45b33fcb2dc4c3b9aae2db5a77137c9a4f
flobeeee <sssver28@gmail.com>
2021-06-10 19:35:48 +0900

    test macbook

---
+++ Level_1/29.desSortNum.js
@@ -0,0 +1,23 @@
+// 문제요약
+// 정수를 입력받아서
+// 내림차순으로 정렬해서 리턴
+
+// 입출력 예시 
+// 118372 =>  873211
+
+// ? 수도코드
+// split 으로 배열로 변경한다.
+// 내림차순으로 sort 를 한다.
+
+// 통과한 코드 (21.06.10)
+// 시간복잡도 O(n) O(n²) O(n log n) / 이유
+function solution(n) {
+    var answer = 0;
+    return answer;
+}
+
+
+// 실행코드
+console.log(solution());
+
+// 링크 : https://programmers.co.kr/learn/courses/30/lessons/12933?language=javascript
