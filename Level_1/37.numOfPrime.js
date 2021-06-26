3263c94a223dbd596838cb4a719882711af94912
flobeeee <sssver28@gmail.com>
2021-06-27 00:37:18 +0900

    add 37 in Level_1

--- Level_1/37.numOfPrime.js
+++ Level_1/37.numOfPrime.js
@@ -6,10 +6,38 @@
 // 5 -> 3
 
 // ? 수도코드
-// 1보다 큰 숫자부터 n까지의 소수를 찾는다
-// 찾은 경우 카운트를 올린다.
+// 숫자를 전부 나열한다.
+// 2부터 시작해서, 2는 소수니까 primeNum 에 넣는다.
+// 2의 배수를 모두 0으로 만든다.
 
-// 통과한 코드 (21.06.24) // 효율성은 통과못함
+// 3차시도 : 에라토스테네스의 체 활용해보기 (21.06.27)
+function solution(n) {
+  let nums = [0, 1]; // 0, 1 index는 사용하지 않음
+  let primeNum = 0
+  // 배열 만들기
+  for (let i = 2; i <= n; i ++) {
+    nums[i] = true;
+  }
+  for (let i = 2; i <=n; i ++) {
+    if (nums[i]){
+      primeNum ++;
+      for (let j = i * i; j <= n; j += i) {
+        nums[j] = false;
+      }
+    }
+  }
+  return primeNum
+}
+
+// 실행코드
+console.log(solution(10));
+
+// 링크 : https://programmers.co.kr/learn/courses/30/lessons/12921
+
+
+
+// 2차시도 : 효율성 점수는 1차보다 좋아졌으나 효율성은 통과못함
+/*
 function checkPrime(n) {
   for(let i = 2; i <= Math.sqrt(n); i ++) {
     if(n % i === 0) {
@@ -27,11 +55,7 @@ function solution(n) {
   }
   return answer;
 }
-
-// 실행코드
-console.log(solution(10));
-
-// 링크 : https://programmers.co.kr/learn/courses/30/lessons/12921
+*/
 
 // 1차시도 : 효율성문제로 3개 테스트항목 통과 X
 /*
